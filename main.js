6d0ee888557abfbebddc892a8aea1b8e12910736
Dawid Janiga <dawid.janiga@netguru.com>
2019-12-14 15:17:08 +0100

    Change content styles

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
-const debug = require('debug')('pomoguru')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
