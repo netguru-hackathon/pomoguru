14fa8f01f9c27a7baa43919e6cc968123d477ba6
Robikos <r.a.kostrzewski@gmail.com>
2019-12-14 14:06:43 +0100

    Add ; everywhere
    #5

--- preload.js
+++ preload.js
@@ -1,75 +1,75 @@
-const createSlack = require('./slack')
+const createSlack = require('./slack');
 
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
   }
 
   for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+    replaceText(`${type}-version`, process.versions[type]);
   }
 
-  const slack = createSlack()
-  const timer = createTimer()
+  const slack = createSlack();
+  const timer = createTimer();
   const pomodoro = createPomodoro({
     slack,
     timer
   })
-})
+});
 
 
 function createTimer() {
-  const oneSecond = 1000
-  const onePomodoroInSeconds = 25 * 60
-  let timerHandler
-  let timeLeft = onePomodoroInSeconds
-  let onTimeChangeListener
+  const oneSecond = 1000;
+  const onePomodoroInSeconds = 25 * 60;
+  let timerHandler;
+  let timeLeft = onePomodoroInSeconds;
+  let onTimeChangeListener;
 
   function start() {
-    console.log('start')
+    console.log('start');
     if (!timerHandler) {
-      timerHandler = setInterval(tickInterval, oneSecond)
+      timerHandler = setInterval(tickInterval, oneSecond);
     }
   }
 
   function pause() {
-    console.log('pause')
-    clearTickInterval()
+    console.log('pause');
+    clearTickInterval();
   }
 
   function stop() {
-    console.log('stop')
-    clearTickInterval()
-    changeTime(onePomodoroInSeconds)
+    console.log('stop');
+    clearTickInterval();
+    changeTime(onePomodoroInSeconds);
   }
 
   function tickInterval() {
-    changeTime(timeLeft - 1)
+    changeTime(timeLeft - 1);
 
     if (timeLeft === 0) {
-      console.log('Pomodoro ends')
-      stop()
+      console.log('Pomodoro ends');
+      stop();
     }
   }
 
   function clearTickInterval() {
-    clearInterval(timerHandler)
-    timerHandler = null
+    clearInterval(timerHandler);
+    timerHandler = null;
   }
 
   function changeTime(value) {
-    timeLeft = value
+    timeLeft = value;
 
     if (onTimeChangeListener) {
-      onTimeChangeListener(timeLeft)
+      onTimeChangeListener(timeLeft);
     }
   }
 
   function onTimeChange(listener) {
-    onTimeChangeListener = listener
+    onTimeChangeListener = listener;
   }
 
   return {
@@ -84,38 +84,38 @@ function createPomodoro({
   slack,
   timer
 }) {
-  const startButton = document.querySelector('#start')
-  const pauseButton = document.querySelector('#pause')
-  const stopButton = document.querySelector('#stop')
-  const timeLeftElement = document.querySelector('#timeLeft')
-  console.log('create Pomodoro')
+  const startButton = document.querySelector('#start');
+  const pauseButton = document.querySelector('#pause');
+  const stopButton = document.querySelector('#stop');
+  const timeLeftElement = document.querySelector('#timeLeft');
+  console.log('create Pomodoro');
 
   startButton.addEventListener('click', () => {
-    timer.start()
-    slack.focusStart()
+    timer.start();
+    slack.focusStart();
   })
 
   pauseButton.addEventListener('click', () => {
-    timer.pause()
-    slack.focusEnd()
+    timer.pause();
+    slack.focusEnd();
   })
 
   stopButton.addEventListener('click', () => {
-    timer.stop()
-    slack.focusEnd()
+    timer.stop();
+    slack.focusEnd();
   })
 
   timer.onTimeChange(timeLeft => {
-    timeLeftElement.innerHTML = formatTime(timeLeft)
+    timeLeftElement.innerHTML = formatTime(timeLeft);
   })
 
   function formatTime(timeInSeconds) {
-    const minutes = Math.floor(timeInSeconds / 60)
-    const seconds = timeInSeconds % 60
-    return `${minutes}:${addLeadingZero(seconds)}`
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+    return `${minutes}:${addLeadingZero(seconds)}`;
   }
 
   function addLeadingZero(value) {
-    return value < 10 ? `0${value}` : value
+    return value < 10 ? `0${value}` : value;
   }
 }
